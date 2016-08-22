d4874e430272379fc000e187d145cf4d21f0f851
Martine Lenders <mail@martine-lenders.eu>
2016-08-22 15:52:13 +0200

    fixup! gnrc_conn: provide port to new API

--- sys/include/net/gnrc/conn.h
+++ sys/include/net/gnrc/conn.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 Martine Lenders <mlenders@inf.fu-berlin.de>
+ * Copyright (C) 2015 Martine Lenders <mlenders@inf.fu-berlin.de>
  *
  * This file is subject to the terms and conditions of the GNU Lesser
  * General Public License v2.1. See the file LICENSE in the top level
@@ -7,8 +7,7 @@
  */
 
 /**
- * @defgroup    net_gnrc_conn   GNRC-specific implementation of the connectivity
- *                              API
+ * @defgroup    net_gnrc_conn   GNRC-specific implementation of the connection API
  * @ingroup     net_gnrc
  * @brief       Provides an implementation of the @ref net_conn by the
  *              @ref net_gnrc
@@ -25,69 +24,105 @@
 
 #include <stdbool.h>
 #include <stdint.h>
-#include "mbox.h"
-#include "net/conn/ep.h"
+#include "net/ipv6/addr.h"
 #include "net/gnrc.h"
-#include "net/gnrc/netreg.h"
+#include "sched.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#ifdef CONN_HAS_CALLBACKS
-#undef CONN_HAS_CALLBACKS   /* not implemented yet */
-#endif
-
-#ifndef CONN_MBOX_SIZE
-#define CONN_MBOX_SIZE      (8)         /**< Size for gnrc_conn_reg_t::mbox_queue */
-#endif
-
 /**
- * @brief   Stack connectivity/registry entry.
+ * @brief   Connection base class
  * @internal
  */
 typedef struct {
-    gnrc_netreg_entry_t entry;          /**< @ref net_gnrc_netreg entry for mbox */
-    mbox_t mbox;                        /**< @ref core_mbox target for the connectivity */
-    msg_t mbox_queue[CONN_MBOX_SIZE];   /**< queue for gnrc_conn_reg_t::mbox */
-} gnrc_conn_reg_t;
+    gnrc_nettype_t l3_type;                     /**< Network layer type of the connection */
+    gnrc_nettype_t l4_type;                     /**< Transport layer type of the connection */
+    gnrc_netreg_entry_t netreg_entry;           /**< @p net_ng_netreg entry for the connection */
+} conn_t;
 
 /**
- * @brief   Raw connectivity type
+ * @brief   Raw connection type
  * @internal
- * @extends gnrc_conn_t
+ * @extends conn_t
  */
 struct conn_ip {
-    gnrc_conn_reg_t reg;                /**< stack connectivity */
-    conn_ep_ip_t local;                 /**< local end-point */
-    conn_ep_ip_t remote;                /**< remote end-point */
+    gnrc_nettype_t l3_type;                     /**< Network layer type of the connection. */
+    gnrc_nettype_t l4_type;                     /**< Transport layer type of the connection.
+                                                 *   Always GNRC_NETTYPE_UNDEF */
+    gnrc_netreg_entry_t netreg_entry;           /**< @p net_ng_netreg entry for the connection */
+    uint8_t local_addr[sizeof(ipv6_addr_t)];    /**< local IP address */
+    size_t local_addr_len;                      /**< length of struct conn_ip::local_addr */
 };
 
 /**
- * @brief   UDP connectivity type
+ * @brief   UDP connection type
  * @internal
- * @extends gnrc_conn_t
+ * @extends conn_t
  */
 struct conn_udp {
-    gnrc_conn_reg_t reg;                /**< stack connectivity */
-    conn_ep_udp_t local;                /**< local end-point */
-    conn_ep_udp_t remote;               /**< remote end-point */
+    gnrc_nettype_t l3_type;                     /**< Network layer type of the connection.
+                                                 *   Always GNRC_NETTYPE_IPV6 */
+    gnrc_nettype_t l4_type;                     /**< Transport layer type of the connection.
+                                                 *   Always GNRC_NETTYPE_UDP */
+    gnrc_netreg_entry_t netreg_entry;           /**< @p net_ng_netreg entry for the connection */
+    uint8_t local_addr[sizeof(ipv6_addr_t)];    /**< local IP address */
+    size_t local_addr_len;                      /**< length of struct conn_ip::local_addr */
 };
 
 /**
- * @brief   Internal helper functions for GNRC
+ * @brief  Bind connection to demux context
+ *
  * @internal
- * @{
+ *
+ * @param[out] entry    @ref net_ng_netreg entry.
+ * @param[in] type      @ref net_ng_nettype.
+ * @param[in] demux_ctx demux context (port or proto) for the connection.
  */
-void gnrc_conn_create(gnrc_conn_reg_t *reg, gnrc_nettype_t type, uint32_t demux_ctx);
-int gnrc_conn_recv(gnrc_conn_reg_t *reg, gnrc_pktsnip_t **pkt, uint32_t timeout,
-                   conn_ep_ip_t *remote);
-int gnrc_conn_send(gnrc_pktsnip_t *payload, conn_ep_ip_t *local,
-                   const conn_ep_ip_t *remote, uint8_t nh);
-/**
- * @}
- */
+static inline void gnrc_conn_reg(gnrc_netreg_entry_t *entry, gnrc_nettype_t type,
+                                 uint32_t demux_ctx)
+{
+    entry->pid = sched_active_pid;
+    entry->demux_ctx = demux_ctx;
+    gnrc_netreg_register(type, entry);
+}
 
+/**
+ * @brief   Sets local address for a connection
+ *
+ * @internal
+ *
+ * @param[out] conn_addr    Pointer to the local address on the connection.
+ * @param[in] addr          An IPv6 address.
+ *
+ * @return  true, if @p addr was a legal address (`::`, `::1` or an address assigned to any
+ *          interface of this node) for the connection.
+ * @return  false if @p addr was not a legal address for the connection.
+ */
+bool gnrc_conn6_set_local_addr(uint8_t *conn_addr, const ipv6_addr_t *addr);
+
+/**
+ * @brief   Generic recvfrom
+ *
+ * @internal
+ *
+ * @param[in] conn      Connection object.
+ * @param[out] data     Pointer where the received data should be stored.
+ * @param[in] max_len   Maximum space available at @p data.
+ * @param[out] addr     NULL pointer or the sender's IP address. Must fit address of connection's
+ *                      family if not NULL.
+ * @param[out] addr_len Length of @p addr. May be NULL if @p addr is NULL.
+ * @param[out] port     NULL pointer or the sender's port.
+ *
+ * @return  The number of bytes received on success.
+ * @return  0, if no received data is available, but everything is in order.
+ * @return  -ENOMEM, if received data was more than max_len.
+ * @returne -ETIMEDOUT, if more than 3 IPC messages were not @ref net_ng_netapi receive commands
+ *          with the required headers in the packet
+ */
+int gnrc_conn_recvfrom(conn_t *conn, void *data, size_t max_len, void *addr, size_t *addr_len,
+                       uint16_t *port);
 
 #ifdef __cplusplus
 }

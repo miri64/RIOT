e960a489c08bc0c756dbc878c4f0ab4b356d8434
Martine Lenders <m.lenders@fu-berlin.de>
2022-12-29 20:01:13 +0100

    Mods to drivers_ws281x

--- tests/driver_ws281x/main.c
+++ tests/driver_ws281x/main.c
@@ -18,95 +18,103 @@
  * @}
  */
 
+#include <stdatomic.h>
 #include <stdio.h>
 
+#include "periph/gpio.h"
 #include "ws281x.h"
 #include "ws281x_params.h"
-#include "xtimer.h"
-
-static const color_rgb_t rainbow[] = {
-    {.r = 0x94, .g = 0x00, .b = 0xd3},
-    {.r = 0x4b, .g = 0x00, .b = 0x82},
-    {.r = 0x00, .g = 0x00, .b = 0xff},
-    {.r = 0x00, .g = 0xff, .b = 0x00},
-    {.r = 0xff, .g = 0xff, .b = 0x00},
-    {.r = 0xff, .g = 0x7f, .b = 0xd3},
-    {.r = 0xff, .g = 0x00, .b = 0x00},
-};
+#include "ztimer.h"
 
 #define RAINBOW_LEN     ARRAY_SIZE(rainbow)
+
+static void _enable_button(void *arg);
+static void _reset_counters(void *arg);
+
+static atomic_uint_fast8_t _value = 2, _left = 0, _right = 0;
+static ztimer_t _debounce_timer = { .callback = _enable_button };
+static ztimer_t _reset_timer = { .callback = _reset_counters };
+
+static void _enable_button(void *arg)
+{
+    gpio_irq_enable((gpio_t)arg);
+}
+
+static void _reset_counters(void *arg)
+{
+    (void)arg;
+    atomic_store(&_left, 0);
+    atomic_store(&_right, 0);
+}
+
+static void _disable_button(void *arg)
+{
+    gpio_irq_disable((gpio_t)arg);
+    _debounce_timer.arg = arg;
+    ztimer_set(ZTIMER_MSEC, &_debounce_timer, 200U);
+}
+
+static void _start_reset_counter(void)
+{
+    ztimer_set(ZTIMER_MSEC, &_reset_timer, 1000);
+}
+
+static void _increment_value(void *arg)
+{
+    uint8_t left = atomic_fetch_add(&_left, 1), right;
+    _disable_button(arg);
+    _start_reset_counter();
+    right = atomic_load(&_right);
+    if (left == 1 && right == 1) {
+        atomic_store(&_value, 2);
+    }
+    else if (_value < 100U) {
+        atomic_fetch_add(&_value, 2);
+    }
+}
+
+static void _decrement_value(void *arg)
+{
+    atomic_fetch_add(&_right, 1);
+    _disable_button(arg);
+    _start_reset_counter();
+    if (_value > 0U) {
+        atomic_fetch_sub(&_value, 2);
+    }
+}
+
 
 int main(void)
 {
     ws281x_t dev;
     int retval;
 
-    puts(
-        "WS281x Test Application\n"
-        "=========================\n"
-        "\n"
-        "If you see an animated rainbow, the driver works as expected.\n"
-        "If the LEDs are flickering, check if the power supply is sufficient\n"
-        "(at least 4V). Also: The logic level has to be at least 0.7 * VDD,\n"
-        "so 3.3V logic with a 5V power supply is out of spec, but might work\n"
-        "OK.\n"
-    );
-
     if (0 != (retval = ws281x_init(&dev, &ws281x_params[0]))) {
         printf("Initialization failed with error code %d\n", retval);
         return retval;
     }
+    gpio_init_int(GPIO2, GPIO_IN_PU, GPIO_FALLING, _increment_value, (void *)GPIO2);
+    gpio_init_int(GPIO8, GPIO_IN_PU, GPIO_FALLING, _decrement_value, (void *)GPIO8);
 
     while (1) {
         unsigned offset = 0;
-        puts("\nAnimation: Moving rainbow...");
-        xtimer_ticks32_t last_wakeup = xtimer_now();
-        for (unsigned i = 0; i < 100; i++) {
+        /* puts("\nAnimation: Moving rainbow..."); */
+        uint32_t last_wakeup = ztimer_now(ZTIMER_MSEC);
+        for (unsigned i = 0; i < (360 * 5); i++) {
             for (uint16_t j = 0; j < dev.params.numof; j++) {
-                ws281x_set(&dev, j, rainbow[(j + offset) % RAINBOW_LEN]);
+                color_hsv_t hsv = {
+                    .h = (((j + i) * 360) / (5 * dev.params.numof)) % 360,
+                    .s = 1,
+                    .v = (float)atomic_load(&_value) / 100,
+                };
+                color_rgb_t col;
+                color_hsv2rgb(&hsv, &col);
+                ws281x_set(&dev, j, col);
             }
             offset++;
             ws281x_write(&dev);
-            xtimer_periodic_wakeup(&last_wakeup, 100 * US_PER_MS);
+            ztimer_periodic_wakeup(ZTIMER_MSEC, &last_wakeup, 100);
         }
-
-        puts("\nAnimation: Fading rainbow...");
-        last_wakeup = xtimer_now();
-        for (unsigned i = 0; i < RAINBOW_LEN; i++) {
-            for (unsigned j = 0; j < 255; j++) {
-                color_rgb_t col;
-                color_rgb_set_brightness(&rainbow[i], &col, j);
-                for (uint16_t k = 0; k < dev.params.numof; k++) {
-                    ws281x_set(&dev, k, col);
-                }
-                ws281x_write(&dev);
-                xtimer_periodic_wakeup(&last_wakeup, 10 * US_PER_MS);
-            }
-            for (unsigned j = 255; j > 0; j--) {
-                color_rgb_t col;
-                color_rgb_set_brightness(&rainbow[i], &col, j);
-                for (uint16_t k = 0; k < dev.params.numof; k++) {
-                    ws281x_set(&dev, k, col);
-                }
-                ws281x_write(&dev);
-                xtimer_periodic_wakeup(&last_wakeup, 10 * US_PER_MS);
-            }
-        }
-
-        puts("\nAnimation: 100 rainbows. (You'll need a long chain for this)");
-        uint8_t buf[RAINBOW_LEN * WS281X_BYTES_PER_DEVICE];
-        for (unsigned i = 0; i < RAINBOW_LEN; i++) {
-            ws281x_set_buffer(buf, i, rainbow[i]);
-        }
-
-        ws281x_prepare_transmission(&dev);
-        for (unsigned i = 0; i < 100; i++) {
-            ws281x_write_buffer(&dev, buf, sizeof(buf));
-        }
-        ws281x_end_transmission(&dev);
-
-        /* wait some time to allow testers to verify the result */
-        xtimer_sleep(5);
     }
 
     return 0;
